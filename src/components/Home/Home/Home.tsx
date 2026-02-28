e712d39fb4c5f7843bb74c0a65fdfbe762bd24d7
kyachingprue <kyachingpruemarma.info@gmail.com>
2026-02-28 14:01:52 +0600

    first commit

---
+++ src/components/Home/Home/Home.tsx
@@ -0,0 +1,12 @@
+import ThemeToggler from '@/components/Helper/ThemeToggler';
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <ThemeToggler/>
+    </div>
+  );
+};
+
+export default Home;
